7a1adf0944fe48ce5b30279cc26f48284002279e
Mirabbos-08 <sobitjonovmirabbos1@gmail.com>
2025-04-18 18:51:25 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import kom from "./assets/k.jpg"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   <header>
+    <div className="container">
+      <img src="https://nextcent-silk.vercel.app/nexcent-logo.png" alt="" />
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Home</a></li>
+          <button>Register Now</button>
+        </ul>
+      </nav>
+    </div>
+   </header>
+   <main>
+    <section className='hero'>
+    <div className="hero container">
+     <div className="hero-text">
+      <h1>Lessons and insights <br /> <span>from 8 years</span></h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, repudiandae!</p>
+      <button>Register</button>
+
+     </div>
+      <div className="hero-img">
+
+      <img src="https://w0.peakpx.com/wallpaper/106/535/HD-wallpaper-pc-gamer.jpg" alt="" />
+      </div>
+    </div>
+    </section>
+    <section className="heros">
+      <div className="clients container">
+        <div className="texts">
+
+        <h2>Our Clients</h2>
+        <p>We have been working with some Fortune 500+ clients</p>
+        </div>
+        <div className="imgs">
+
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/3a47f248449813.5608313a6db56.png" alt="" />
+        </div>
+      </div>
+    </section>
+    <section className="Your">
+      <h3>Manage your entire community <br /> in a single system</h3>
+      <div className="ota">
+
+      <div className="card">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/006/943/215/small/group-icon-sign-symbol-logo-free-vector.jpg" alt="" />
+      <h4>Membership <br />Organisations </h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo accusamus voluptatibus hic animi ullam atque qui pariatur maiores ea tempore nemo assumenda</p>
+      </div>
+      <div className="card">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/006/943/215/small/group-icon-sign-symbol-logo-free-vector.jpg" alt="" />
+      <h4>Membership <br />Organisations </h4>
+      <p className="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo accusamus voluptatibus hic animi ullam atque qui pariatur maiores ea tempore nemo assumenda</p>
+      </div>
+      <div className="card">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/006/943/215/small/group-icon-sign-symbol-logo-free-vector.jpg" alt="" />
+      <h4>Membership <br />Organisations </h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo accusamus voluptatibus hic animi ullam atque qui pariatur maiores ea tempore nemo assumenda</p>
+      </div>
+
+      </div>
+
+    </section>
+    <section className='hero'>
+    <div className="hero container">
+    <div className="hero-img">
+
+<img src="https://w0.peakpx.com/wallpaper/106/535/HD-wallpaper-pc-gamer.jpg" alt="" />
+</div>
+     <div className="hero-text">
+      <h1>Lessons and insights <br /> <span>from 8 years</span></h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, repudiandae!</p>
+      <button>Register</button>
+
+     </div>
+      
+    </div>
+    </section>
+   </main>
+    </>
+  )
+}
+
+export default App
